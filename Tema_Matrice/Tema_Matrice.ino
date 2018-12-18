96360878c05dbdefa35d059c0643f9b763303549
MoldovanG <george.alexandru.moldovan@gmail.com>
2018-12-18 15:03:46 +0200

    Am sters o parte din cod deoarece era redundant si am adus modificari minore in rest

--- Tema_Matrice/Tema_Matrice.ino
+++ Tema_Matrice/Tema_Matrice.ino
@@ -12,6 +12,11 @@
 #define INITIAL_BOARDLENGHT 4
 #define LIMIT_LINE 6
 #define START_INDEX 0
+#define SCREEN_DIMENSION 9
+#define SPEED_INCREASE_NUMBER 5
+#define LENGHT_DECREASE_NUMBER 10
+#define MARGIN 8
+#define START 0
 
 #define RS 2
 #define E 3
@@ -35,22 +40,9 @@ LedControl lc = LedControl( MAX7219_1, CLK_PIN, LOAD_PIN , NR_DRIVER ); //DIN, C
  int boardLenght; // the lenght of the catch block, automatically gets shorter every 10 points
  int score;
  long long lastMove;
- int playScreen [9][9];
+ int playScreen [SCREEN_DIMENSION][SCREEN_DIMENSION];
  int SPEED;
  
- byte number_0_Sad[] = { B00100100,B00100100,B00000000,B00011000,B00000000,B00011000,B00100100,B01000010};
- byte number_1[] = { B00000000,B00011000,B00111000,B01111000,B00011000,B00011000,B00011000,B00011000};
- byte number_2[] =   { B00111110,B00111110,B00000110,B00111110,B00110000,B00110000,B00111110,B00111110};
- byte number_3[] =  { B00000000,B00111100,B00111100,B00001100,B00111100,B00001100,B00111100,B00111100};
- byte number_4 [] = { B01100110,B01100110,B01100110,B01111110,B00000110,B00000110,B00000110,B00000110};
- byte number_5 [] = { B00000000,B01111110,B01111110,B01100000,B01111110,B00000110,B01111110,B01111110};
- byte number_6 [] = { B00111110,B00111110,B00110000,B00110000,B00111110,B00111110,B00110110,B00111110};
- byte number_7 [] = { B00011110,B00011110,B00000110,B00000110,B00011111,B00000110,B00000110,B00000110};
- byte number_8 [] = { B00111110,B00100010,B00100010,B00111110,B00111110,B00100010,B00100010,B00111110};
- byte number_9 [] = { B01111000,B01001000,B01001000,B01111000,B00001000,B00001000,B00001000,B00001000};
- byte number_10 [] = { B00100000,B01101110,B10101010,B00101010,B00101010,B00101110,B00000000,B00000000};
- byte numberG_10_Smile [] = { B00100100,B00100100,B00000000,B00011000,B00000000,B01000010,B00100100,B00011000};
- byte numberG_10_Amazed [] = { B00100100,B00100100,B00000000,B00011000,B00000000,B00011000,B00100100,B00011000};
 
  bool startScreen;
  bool firstTime;
@@ -68,18 +60,18 @@ void setup()
   ballColumn = rand() % LIMIT_LINE + 1;
  
   SPEED = 1000;
-  for (int i = 0 ; i < 8 ; i++)
-  for (int j = 0 ; j<9 ; j++)
+  for (int i = START; i < SCREEN_DIMENSION; i++)
+  for (int j = START; j < SCREEN_DIMENSION; j++)
    playScreen[i][j] = 0;
 
    
   
-   pinMode(PWM , OUTPUT);
-   analogWrite(PWM , 95);
+   pinMode(PWM, OUTPUT);
+   analogWrite(PWM, 95);
    
-   lcd.begin (16 , 2) ;
+   lcd.begin (16, 2) ;
    lcd.clear();
-   lcd.setCursor(0 , 0);
+   lcd.setCursor(0, 0);
    
   boardLenght = INITIAL_BOARDLENGHT;
   startScreen = true;
@@ -95,8 +87,8 @@ void reviveBall () {
         
 void ballCatched () {
       score++;
-      if (score % 5 == 0) SPEED -= 100;
-      if (score % 10 == 0) boardLenght--;
+      if (score % SPEED_INCREASE_NUMBER == 0) SPEED -= 100;
+      if (score % LENGHT_DECREASE_NUMBER == 0) boardLenght--;
       reviveBall();
       
     }
@@ -105,94 +97,18 @@ void moveBall(){
         ballLine++;    
     }   
 
-void pause (int time)
-   {
-    int lastUpdate=millis();
-    while (millis()-lastUpdate<time);
-      
-   }
+
    
 void endGame()
   {
-    if (score > highScore ) {highScore = score; EEPROM.write (0, score);}
-    lc.clearDisplay(0);
-    switch (score){
-      case 0:   
-      {
-        printByte(number_0_Sad);
-        break;
-      }
-      case 1: 
-      {
-        printByte(number_1);
-        break;
-      }
-      case 2: 
-      {
-        printByte(number_2);
-        break;
-      }
-      case 3 : 
-      {
-        printByte(number_3);
-        break;
-      }
-      case 4 : 
-      {
-        printByte(number_4);
-        break;
-      }
-      case 5 : 
-      {
-        printByte(number_5);
-        break;
-      }
-      case 6 : 
-      {
-        printByte(number_6);
-        break;
-      }
-      case 7 : 
-      {
-        printByte(number_7);
-        break;
-      }
-      case 8 : 
-      {
-        printByte(number_8);
-        break;
-      }
-      case 9 :
-      {
-        printByte(number_9);
-        break;
-      }
-      case 10 : 
-      {
-        printByte(number_10);
-        break;
-      }
-                                                
-    
-      default :
-      {
-        int counter = 0;
-        while (counter < 5) {
-        printByte(numberG_10_Smile);
-        
-        pause(500);
-       
-        lc.clearDisplay(0);
-        printByte(numberG_10_Amazed);
-        
-        pause(500);
-   
-        counter++;
-        }
-      }
+    if (score > highScore ) 
+    {
+      highScore = score; 
+      EEPROM.write (0, score);
     }
+    lc.clearDisplay(0);
+    
     startScreen = true;
-    pause(1500);
     initialiseScreenAndValues();
    
   }
@@ -201,10 +117,9 @@ void setPlayerOnMap (int playerPosition)
   {
     playScreen[LIMIT_LINE][playerPosition + boardLenght] = playScreen[LIMIT_LINE][playerPosition] = 1; //setez marginile cutiei 
     int i;
-    for (i = playerPosition ; i <= playerPosition + boardLenght ; i++)
+    for (i = playerPosition; i <= playerPosition + boardLenght; i++)
     playScreen[LIMIT_LINE+1][i] = 1; //setez fundul cutiei
-   
-  }
+   }
   
 void setPlayerPosition()
   {
@@ -215,8 +130,8 @@ void setPlayerPosition()
 
 void clearMap()
   {
-    for (int i = 0 ; i < 8 ; i++)
-    for (int j = 0 ; j < 8 ; j++)
+    for (int i = START; i < MARGIN; i++)
+    for (int j = START; j < MARGIN; j++)
     playScreen[i][j] = 0;
   }
   
@@ -269,8 +184,8 @@ void loop()
   
 void printByte (byte character [])
     {
-      int i = 0;
-      for (i = 0; i < 8; i++)
+      int i;
+      for (i = START; i < MARGIN; i++)
         {
           lc.setRow(0, i, character[i]);
         }
@@ -281,8 +196,8 @@ void printByte (byte character [])
 void draw ()
     {
       lc.clearDisplay(0);
-    for (int row = 0; row < 8; row++)
-      for (int col = 0; col < 8; col++)
+    for (int row = START; row < MARGIN; row++)
+      for (int col = START; col < MARGIN; col++)
          if (playScreen[col][row] == 1)
             lc.setLed(0, col, row, true);
     }
